a071b46b3763afc29994deeed2383ede845ebc0d
Zeyu Keith Fu <43019187+ZeyuKeithFu@users.noreply.github.com>
2019-02-27 16:23:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import re_encode
 import generate_video
 from multiprocessing import Queue
-import time
-import sys
+import os
 
+INP_PATH = '/Users/aznable/Desktop/exercise-2-ffmpeg-ZeyuKeithFu/videos/'
+MAX_COUNT = 3
+
+def main():
+    q = Queue(3)
+    inputs = []
+    for i in os.listdir(INP_PATH):
+        if i != '.DS_Store':
+            inputs.append(INP_PATH + i)
+
+    for j in range(len(inputs)):
+        q.put(j + 1, block=True)
+        t = re_encode.re_encode(q, j + 1, inputs[j])
+        t.start()
+
+
+if __name__ == '__main__':
+    main()
