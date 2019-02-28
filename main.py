71851169162245740320984604561b8723b58cac
Zeyu Keith Fu <43019187+ZeyuKeithFu@users.noreply.github.com>
2019-02-27 21:29:46 -0500

    Concurrently processing with max threads

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import re_encode
+import threading
 import generate_video
 from multiprocessing import Queue
 import os
 
-INP_PATH = '/Users/aznable/Desktop/exercise-2-ffmpeg-ZeyuKeithFu/videos/'
-MAX_COUNT = 3
+INP_PATH = '../videos/' # Input videos directory
+MAX_COUNT = 5 # Maximum threads
 
 def main():
-    q = Queue(3)
+
+    q = Queue()
     inputs = []
+
     for i in os.listdir(INP_PATH):
-        if i != '.DS_Store':
+        if (i != '.DS_Store') & (len(i.split("_")) == 1):
             inputs.append(INP_PATH + i)
 
     for j in range(len(inputs)):
-        q.put(j + 1, block=True)
-        t = re_encode.re_encode(q, j + 1, inputs[j])
-        t.start()
+        q.put(j+1)
+
+    while True:
+        if threading.active_count() < MAX_COUNT:
+            id = q.get()
+            thread = re_encode.re_encode(id, inputs[id-1])
+            thread.start()
+        elif q.empty():
+            break;
+
+    return
 
 
 if __name__ == '__main__':
