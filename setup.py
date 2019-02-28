82929e59a569b24d9d8014f99a4fc52271412388
Zeyu Keith Fu <43019187+ZeyuKeithFu@users.noreply.github.com>
2019-02-27 22:27:25 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='exercise-2-ffmpeg-ZeyuKeithFu',
+    version='',
+    url='',
+    license='MIT',
+    author='Zeyu Fu',
+    author_email='zeyufu@bu.edu',
+    description='',
+    install_requires=['flake8','pytest'],
+)
